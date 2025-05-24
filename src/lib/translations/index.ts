b2da3f070b91bc67a832bacda5052fb308c6f2bf
Oded2 <111157775+Oded2@users.noreply.github.com>
2025-05-24 13:22:15 +0300

    Removed unnecessary file

--- src/lib/translations/index.ts
+++
@@ -1,9 +0,0 @@
-import { register, init, getLocaleFromNavigator } from 'svelte-i18n';
-
-register('en', () => import('./en.json'));
-register('he', () => import('./he.json'));
-
-init({
-	fallbackLocale: 'en',
-	initialLocale: getLocaleFromNavigator()
-});
