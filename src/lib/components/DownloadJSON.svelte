4be9e189d1821f1e8cb52a1919f29fa25551a5bd
Oded2 <111157775+Oded2@users.noreply.github.com>
2025-05-25 17:38:44 +0300

    Fixed JSON download

--- src/lib/components/DownloadJSON.svelte
+++ src/lib/components/DownloadJSON.svelte
@@ -6,7 +6,7 @@
 		className,
 		children
 	}: { data: { [key: string]: any }; className?: string; children: Snippet } = $props();
-	const jsonHref = `data:application/json;charset=utf-8,${encodeURIComponent(JSON.stringify(data, null, 2))}`;
+	const jsonHref = `data:application/json;charset=utf-8,${JSON.stringify(data, null, 2)}`;
 </script>
 
 <a href={jsonHref} download="plan.json" class={className}>{@render children()}</a>
