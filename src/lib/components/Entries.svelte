15ad064759b3f2fee824cf10cd3e02289b62553f
Oded2 <111157775+Oded2@users.noreply.github.com>
2025-05-29 02:40:41 +0300

    Set table max-height to fill remaining space between the navbar and viewport

--- src/lib/components/Entries.svelte
+++ src/lib/components/Entries.svelte
@@ -1,16 +1,42 @@
 <script lang="ts">
 	import { t } from '$lib/stores/localization';
+	import { onMount } from 'svelte';
 	import EntryForm from './EntryForm.svelte';
 	import Table from './Table.svelte';
+
+	const XLBREAKPOINT = 1280;
+
+	let refDiv: HTMLDivElement;
+	let xlContainer: HTMLDivElement;
+
+	let ready = $state(false);
+
+	function updateTable() {
+		requestAnimationFrame(() => {
+			ready = false;
+			const height = refDiv.getBoundingClientRect().height;
+			if (window.innerWidth > XLBREAKPOINT) xlContainer.style.maxHeight = `${height}px`;
+			else xlContainer.style.maxHeight = `99999px`;
+			ready = true;
+		});
+	}
+
+	onMount(() => {
+		updateTable();
+		window.addEventListener('resize', updateTable);
+		return () => window.removeEventListener('resize', updateTable);
+	});
 </script>
 
-<div class="flex grid-cols-6 flex-col gap-4 xl:grid" dir="auto">
+<div bind:this={refDiv} class="flex grow grid-cols-6 flex-col gap-4 xl:grid" dir="auto">
 	<!-- Placeholder text in order for dir="auto" to work properly -->
 	<h1 class="hidden">{$t('welcome')}</h1>
-	<div class="w-full overflow-auto xl:col-span-2 print:hidden">
+	<div class="w-full overflow-auto xl:col-span-2">
 		<EntryForm></EntryForm>
 	</div>
-	<div class="mx-auto flex w-full flex-col gap-4 xl:col-span-4 print:col-span-full">
-		<Table editable></Table>
+	<div bind:this={xlContainer} class="mx-auto flex w-full xl:col-span-4">
+		{#if ready}
+			<Table editable></Table>
+		{/if}
 	</div>
 </div>
